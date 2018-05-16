fcb031ae0d7630c884884526697a614d364cf164
Luis Bastidas <luisbastir@gmail.com>
2018-05-16 17:38:29 -0500

    Add remove friends, search friends. show friend profile and show stocks that user is tracking

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: 'users#search'
+  get 'my_friends', to: 'users#my_friends'
+  post 'add_friend', to: 'users#add_friend'
 end
