6081a899df4f09ceaca019c08600775b13c19aa5
Luis Bastidas <luisbastir@gmail.com>
2018-08-22 12:17:20 -0500

    My Portflolio handled by Angular JS

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
-  has_many :users, through: :user_stocks
+  has_many :users, through: :user_stock
+  
+  attr_accessor :can_be_added
   
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
